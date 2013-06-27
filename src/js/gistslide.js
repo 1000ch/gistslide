3368f2ed8834a97d7c8bb472a5899bab50047072
1000ch <orz1000ch@gmail.com>
2013-06-28 01:52:40 +0900

    refactoring

--- src/js/gistslide.js
+++ src/js/gistslide.js
@@ -4,6 +4,14 @@
     var doc = win.document;
     var slice = [].slice;
 
+	var qs = doc.querySelector;
+	var qsa = doc.querySelectorAll;
+
+	var KEYCODE_LEFT = 37;
+	var KEYCODE_UP = 38;
+	var KEYCODE_RIGHT = 39;
+	var KEYCODE_BOTTOM = 40;
+
     function createNode(tagName, param) {
         var node = doc.createElement(tagName);
         var key;
@@ -13,49 +21,56 @@
         return node;
     }
 
-    var headerPosition = [];
-    var currentSlideIndex = 0;
-
-    function keydownEventHandler(e) {
-        if(e.which == 37) {
-            if(0 < currentSlideIndex) {
-                currentSlideIndex--;
-            } else {
-                currentSlideIndex = 0;
-            }
-            slideTo(currentSlideIndex);
-        } else if(e.which == 39) {
-            if(currentSlideIndex < headerPosition.length) {
-                currentSlideIndex++;
-            } else {
-                currentSlideIndex = headerPosition.length;
-            }
-            slideTo(currentSlideIndex);
-        }
-    }
-
-    function slideTo(index) {
-        if(index < 0 || headerPosition.length < index) {
-            window.scrollTo(0, 0);
-        } else {
-            window.scrollTo(0, headerPosition[index]);
-        }
-    }
+	function SlideContainer(headerElements) {
+		this.headerElements = headerElements;
+		this.headerOffsets = [];
+		for(var i = 0, len = this.headerElements.length;i < len;i++) {
+			this.headerOffsets.push(this.headerElements[i].offsetTop);
+		}
+		this.headerOffsets.sort(function(x, y) {
+			return (x > y);
+		});
+		this.limitIndex = this.headerElements.length;
+		this.currentIndex = 0;
+		this.slideTo = function(index) {
+			if(index < 0) {
+				window.scrollTo(0, 0);
+			} else if(this.limitIndex < index) {
+				window.scrollTo(0, this.headerOffsets[this.limitIndex]);
+			} else {
+				window.scrollTo(0, this.headerOffsets[index]);
+			}
+		};
+		this.prev = function() {
+			this.currentIndex--;
+			if(this.currentIndex < 0) {
+				this.currentIndex = 0;
+			}
+			this.slideTo(this.currentIndex);
+		};
+		this.next = function() {
+			this.currentIndex++;
+			if(this.limitIndex < this.currentIndex) {
+				this.currentIndex = this.limitIndex;
+			}
+			this.slideTo(this.currentIndex);
+		};
+	}
 
     function initializeGist() {
         //hide left panel
-        doc.querySelector(".root-pane").style.display = "none";
+        qs(".root-pane").style.display = "none";
 
         //fix main content
-        var column = doc.querySelector(".column");
+        var column = qs(".column");
         column.style.float = "none";
         column.style.width = "100%";
 
         //hide other content
-        doc.querySelector(".js-comment-form").style.display = "none";
-        doc.getElementById("header").style.display = "none";
-        doc.querySelector(".pagehead").style.display = "none";
-        doc.getElementById("footer").style.display = "none";
+        qs(".js-comment-form").style.display = "none";
+        qs("#header").style.display = "none";
+        qs(".pagehead").style.display = "none";
+        qs("#footer").style.display = "none";
     }
 
     (function() {
@@ -71,21 +86,23 @@
         });
 
         //insert nodes into head tail
-        //doc.querySelector('head').appendChild(css);
+        //qs('head').appendChild(css);
 
         initializeGist();
 
         //cache header positions
-        var header1 = slice.call(doc.getElementsByTagName('h1'));
-        var header2 = slice.call(doc.getElementsByTagName('h2'));
-        var headerArray = header1.concat(header2);
-        for(var i = 0, len = headerArray.length;i < len;i++) {
-            headerPosition.push(headerArray[i].offsetTop);
-        }
-        headerPosition.sort();
+        var header1 = slice.call(qsa('h1'));
+        var header2 = slice.call(qsa('h2'));
+		var container = new SlideContainer(header1.concat(header2));
 
         //listen keydown event
-        doc.addEventListener('keydown', keydownEventHandler);
+        doc.addEventListener('keydown', function(e) {
+			if(e.keyCode == KEYCODE_LEFT) {
+				container.prev();
+			} else if(e.keyCode == KEYCODE_RIGHT) {
+				container.next();
+			}
+		});
     })();
 
 })();
