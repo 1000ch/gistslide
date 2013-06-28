d92891be1e205104e86f99184b7db1c7790b577a
hiloki <service@inkdesign.jp>
2013-06-28 13:27:56 +0900

    Add CSS/Sass & Styleguide template

--- gruntfile.js
+++ gruntfile.js
@@ -6,16 +6,34 @@ module.exports = function(grunt) {
 		csslint: {
 			all: ["src/css/gistslide.css"]
 		},
+		sass: {
+			options: {
+				style: 'expanded'
+			},
+			compile: {
+				files: {
+				'src/css/themes/simple.css': ['src/sass/themes/simple.scss']
+				}
+			}
+		},
 		watch: {
-			files: ["src/js/gistslide.js"],
-			tasks: ["jshint"]
+			js: {
+				files: ["src/js/gistslide.js"],
+				tasks: ["jshint"]
+			},
+			css: {
+				files: ["src/sass/**/*.scss"],
+				tasks: ["sass"]
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-livereload');
 
 	grunt.registerTask("default", "watch");
+	grunt.registerTask("css", ["sass"]);
 };
