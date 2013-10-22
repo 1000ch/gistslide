44968b1609caa733745864c07e48b4eb42d841f5
1000ch <orz1000ch@gmail.com>
2013-10-22 12:38:20 +0900

    fix indent

--- gruntfile.js
+++ gruntfile.js
@@ -1,66 +1,66 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		jshint: {
-			all: ["src/js/gistslide.js"]
-		},
-		uglify: {
-			all: {
-				files: {
-					"src/js/gistslide.min.js": ["src/js/gistslide.js"]
-				}
-			}
-		},
-		csslint: {
-			all: ["src/css/themes/*.css"]
-		},
-		sass: {
-			// options: {
-			// 	style: 'expanded'
-			// },
-			compile: {
-				files: {
-				'src/css/themes/default.css': ['src/sass/themes/default.scss'],
-				'src/css/themes/simple.css': ['src/sass/themes/simple.scss'],
-				'src/css/themes/night.css': ['src/sass/themes/night.scss'],
-				'src/css/themes/redhot.css': ['src/sass/themes/redhot.scss'],
-				'src/css/themes/green.css': ['src/sass/themes/green.scss']
-				}
-			}
-		},
-		csso: {
-			option: {
-				report: 'min'
-			},
-			dist: {
-				files: {
-				'src/css/themes/default.css': ['src/css/themes/default.css'],
-				'src/css/themes/simple.css': ['src/css/themes/simple.css'],
-				'src/css/themes/night.css': ['src/css/themes/night.css'],
-				'src/css/themes/redhot.css': ['src/css/themes/redhot.css'],
-				'src/css/themes/green.css': ['src/css/themes/green.css']
-				}
-			}
-		},
-		watch: {
-			js: {
-				files: ["src/js/gistslide.js"],
-				tasks: ["jshint", "uglify"]
-			},
-			css: {
-				files: ["src/sass/**/*.scss"],
-				tasks: ["sass","csso"]
-			}
-		}
-	});
+  grunt.initConfig({
+    jshint: {
+      all: ["src/js/gistslide.js"]
+    },
+    uglify: {
+      all: {
+        files: {
+          "src/js/gistslide.min.js": ["src/js/gistslide.js"]
+        }
+      }
+    },
+    csslint: {
+      all: ["src/css/themes/*.css"]
+    },
+    sass: {
+      // options: {
+      // 	style: 'expanded'
+      // },
+      compile: {
+        files: {
+          'src/css/themes/default.css': ['src/sass/themes/default.scss'],
+          'src/css/themes/simple.css': ['src/sass/themes/simple.scss'],
+          'src/css/themes/night.css': ['src/sass/themes/night.scss'],
+          'src/css/themes/redhot.css': ['src/sass/themes/redhot.scss'],
+          'src/css/themes/green.css': ['src/sass/themes/green.scss']
+        }
+      }
+    },
+    csso: {
+      option: {
+        report: 'min'
+      },
+      dist: {
+        files: {
+          'src/css/themes/default.css': ['src/css/themes/default.css'],
+          'src/css/themes/simple.css': ['src/css/themes/simple.css'],
+          'src/css/themes/night.css': ['src/css/themes/night.css'],
+          'src/css/themes/redhot.css': ['src/css/themes/redhot.css'],
+          'src/css/themes/green.css': ['src/css/themes/green.css']
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: ["src/js/gistslide.js"],
+        tasks: ["jshint", "uglify"]
+      },
+      css: {
+        files: ["src/sass/**/*.scss"],
+        tasks: ["sass","csso"]
+      }
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-csslint');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-csso');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-livereload');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-csso');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
 
-	grunt.registerTask("default", "watch");
-	grunt.registerTask("css", "watch");
+  grunt.registerTask("default", "watch");
+  grunt.registerTask("css", "watch");
 };
--- src/js/gistslide.js
+++ src/js/gistslide.js
@@ -1,319 +1,315 @@
 (function() {
 
-	//cache external variable
-	var win = window;
-	var doc = win.document;
+  //cache external variable
+  var win = window;
+  var doc = win.document;
 
-	//native alias
-	var forEach = [].forEach;
-	var slice = [].slice;
-	var map = [].map;
-	var filter = [].filter;
+  //native alias
+  var forEach = [].forEach;
+  var slice = [].slice;
+  var map = [].map;
+  var filter = [].filter;
 
-	//constant
-	var KEYCODE_ENTER = 13;
-	var KEYCODE_LEFT = 37;
-	var KEYCODE_UP = 38;
-	var KEYCODE_RIGHT = 39;
-	var KEYCODE_BOTTOM = 40;
+  //constant
+  var KEYCODE_ENTER = 13;
+  var KEYCODE_LEFT = 37;
+  var KEYCODE_UP = 38;
+  var KEYCODE_RIGHT = 39;
+  var KEYCODE_BOTTOM = 40;
 
-	/**
-	 * querySelector alias
-	 * @param {string} selector
-	 * @param {HTMLElement} context
-	 * @returns {Node}
-	 */
-	var qs = function(selector, context) {
-		return (context || doc).querySelector(selector);
-	};
+  /**
+   * querySelector alias
+   * @param {string} selector
+   * @param {HTMLElement} context
+   * @returns {Node}
+   */
+  var qs = function(selector, context) {
+    return (context || doc).querySelector(selector);
+  };
 
-	/**
-	 * document.querySelectorAll alias
-	 * @param {string} selector
-	 * @param {HTMLElement} context
-	 * @returns {NodeList}
-	 */
-	var qsa = function(selector, context) {
-		return (context || doc).querySelectorAll(selector);
-	};
+  /**
+   * document.querySelectorAll alias
+   * @param {string} selector
+   * @param {HTMLElement} context
+   * @returns {NodeList}
+   */
+  var qsa = function(selector, context) {
+    return (context || doc).querySelectorAll(selector);
+  };
 
-	/**
-	 * document.getElementById alias
-	 * @param {string} selector
-	 * @param {HTMLElement} context
-	 * @returns {NodeList}
-	 */
-	var byId = function(id) {
-		return document.getElementById(id);
-	};
+  /**
+   * document.getElementById alias
+   * @param {string} selector
+   * @param {HTMLElement} context
+   * @returns {NodeList}
+   */
+  var byId = function(id) {
+    return document.getElementById(id);
+  };
 
-/**
-	 * getElementsByClassName alias
-	 * @param {string} selector
-	 * @param {HTMLElement} context
-	 * @returns {Node}
-	 */
-	var byClass = function(className, context) {
-		return (context || document).getElementsByClassName(className);
-	};
+  /**
+   * getElementsByClassName alias
+   * @param {string} selector
+   * @param {HTMLElement} context
+   * @returns {Node}
+   */
+  var byClass = function(className, context) {
+    return (context || document).getElementsByClassName(className);
+  };
 
-	/**
-	 * getElementsByTagName alias
-	 * @param {string} tagName
-	 * @param {HTMLElement} context
-	 * @returns {Node}
-	 */
-	var byTag = function(tagName, context) {
-		return (context || document).getElementsByTagName(tagName);
-	};
+  /**
+   * getElementsByTagName alias
+   * @param {string} tagName
+   * @param {HTMLElement} context
+   * @returns {Node}
+   */
+  var byTag = function(tagName, context) {
+    return (context || document).getElementsByTagName(tagName);
+  };
 
-	/**
-	 * create node
-	 * @param {string} tagName
-	 * @param {object} param
-	 * @returns {HTMLElement}
-	 */
-	function createNode(tagName, param) {
-		var node = doc.createElement(tagName);
-		var key;
-		for(key in param) {
-			node.setAttribute(key, param[key]);
-		}
-		return node;
-	}
+  /**
+   * create node
+   * @param {string} tagName
+   * @param {object} param
+   * @returns {HTMLElement}
+   */
+  function createNode(tagName, param) {
+    var node = doc.createElement(tagName);
+    var key;
+    for(key in param) {
+      node.setAttribute(key, param[key]);
+    }
+    return node;
+  }
 
-	/**
-	 * wrap node
-	 * @param {HTMLElement} targetNode
-	 * @param {HTMLElement} destNode
-	 */
-	function wrapNode(targetNode, destNode) {
-		//append clone of targetNode to wrapNode
-		destNode.appendChild(targetNode.cloneNode(true));
-		//append cloned node and remove before node
-		var parentNode = targetNode.parentNode;
-		parentNode.insertBefore(destNode, targetNode);
-		parentNode.removeChild(targetNode);
-	}
+  /**
+   * wrap node
+   * @param {HTMLElement} targetNode
+   * @param {HTMLElement} destNode
+   */
+  function wrapNode(targetNode, destNode) {
+    //append clone of targetNode to wrapNode
+    destNode.appendChild(targetNode.cloneNode(true));
+    //append cloned node and remove before node
+    var parentNode = targetNode.parentNode;
+    parentNode.insertBefore(destNode, targetNode);
+    parentNode.removeChild(targetNode);
+  }
 
-	/**
-	 * move node
-	 * @param {HTMLElement} targetNode
-	 * @param {HTMLElement} destNode
-	 */
-	function moveNode(targetNode, destNode) {
-		//append clone of targetNode to destNode
-		destNode.appendChild(targetNode.cloneNode(true));
-		//remove itself
-		removeNode(targetNode);
-	}
+  /**
+   * move node
+   * @param {HTMLElement} targetNode
+   * @param {HTMLElement} destNode
+   */
+  function moveNode(targetNode, destNode) {
+    //append clone of targetNode to destNode
+    destNode.appendChild(targetNode.cloneNode(true));
+    //remove itself
+    removeNode(targetNode);
+  }
 
-	/**
-	 * remove node
-	 * @param {HTMLElement} targetNode
-	 */
-	function removeNode(targetNode) {
-		if(targetNode && targetNode.parentNode) {
-			targetNode.parentNode.removeChild(targetNode);
-		}
-	}
+  /**
+   * remove node
+   * @param {HTMLElement} targetNode
+   */
+  function removeNode(targetNode) {
+    if(targetNode && targetNode.parentNode) {
+      targetNode.parentNode.removeChild(targetNode);
+    }
+  }
 
-	/**
-	 * get sibling
-	 * @param {HTMLElement} targetNode
-	 * @returns {HTMLElement}
-	 */
-	function getSibling(targetNode) {
-		var node = targetNode;
-		while ((node = node.nextSibling) && node.nodeType !== 1) {}
-		return node;
-	}
+  /**
+   * get sibling
+   * @param {HTMLElement} targetNode
+   * @returns {HTMLElement}
+   */
+  function getSibling(targetNode) {
+    var node = targetNode;
+    while ((node = node.nextSibling) && node.nodeType !== 1) {}
+    return node;
+  }
 
-	/**
-	 * add class to node
-	 * @param {HTMLElement} targetNode
-	 * @param {string} className
-	 */
-	function addClass(targetNode, className) {
-		if(targetNode) {
-			var classArray = className.split(" ");
-			forEach.call(classArray, function(name) {
-				targetNode.classList.add(name);
-			});
-		}
-	}
+  /**
+   * add class to node
+   * @param {HTMLElement} targetNode
+   * @param {string} className
+   */
+  function addClass(targetNode, className) {
+    if(targetNode) {
+      var classArray = className.split(" ");
+      forEach.call(classArray, function(name) {
+        targetNode.classList.add(name);
+      });
+    }
+  }
 
-	/**
-	 * remove class from node
-	 * @param {HTMLElement} targetNode
-	 * @param {string} className
-	 */
-	function removeClass(targetNode, className) {
-		if(targetNode) {
-			var classArray = className.split(" ");
-			forEach.call(classArray, function(name) {
-				targetNode.classList.remove(name);
-			});
-		}
-	}
+  /**
+   * remove class from node
+   * @param {HTMLElement} targetNode
+   * @param {string} className
+   */
+  function removeClass(targetNode, className) {
+    if(targetNode) {
+      var classArray = className.split(" ");
+      forEach.call(classArray, function(name) {
+        targetNode.classList.remove(name);
+      });
+    }
+  }
 
-	/**
-	 * slide container
-	 * @param elementList
-	 * @constructor
-	 */
-	function SlideContainer(elementList) {
-		this.elementList = slice.call(elementList);
-		this.elementList.sort(function(x, y) {
-			return parseInt(x.offsetTop, 10) > parseInt(y.offsetTop, 10);
-		});
-		this.offsetList = map.call(this.elementList, function(element) {
-			return element.offsetTop;
-		});
-		this.offsetList.sort(function(x, y) {
-			return parseInt(x, 10) - parseInt(y, 10);
-		});
-		this.limitIndex = this.elementList.length - 1;
-		this.currentIndex = 0;
-		/**
-		 * slide to index
-		 * @param {number} index
-		 */
-		this.slideTo = function(index) {
-			if(index < 0) {
-				window.scrollTo(0, 0);
-			} else if(this.limitIndex < index) {
-				window.scrollTo(0, this.offsetList[this.limitIndex]);
-			} else {
-				window.scrollTo(0, this.offsetList[index]);
-			}
-		};
-		/**
-		 * slide to prev
-		 */
-		this.prev = function() {
-			this.currentIndex--;
-			if(this.currentIndex < 0) {
-				this.currentIndex = 0;
-			}
-			this.slideTo(this.currentIndex);
-		};
-		/**
-		 * slide to prev
-		 */
-		this.next = function() {
-			this.currentIndex++;
-			if(this.limitIndex < this.currentIndex) {
-				this.currentIndex = this.limitIndex;
-			}
-			this.slideTo(this.currentIndex);
-		};
-	}
+  /**
+   * slide container
+   * @param elementList
+   * @constructor
+   */
+  function SlideContainer(elementList) {
+    this.elementList = slice.call(elementList);
+    this.elementList.sort(function(x, y) {
+      return parseInt(x.offsetTop, 10) > parseInt(y.offsetTop, 10);
+    });
+    this.offsetList = map.call(this.elementList, function(element) {
+      return element.offsetTop;
+    });
+    this.offsetList.sort(function(x, y) {
+      return parseInt(x, 10) - parseInt(y, 10);
+    });
+    this.limitIndex = this.elementList.length - 1;
+    this.currentIndex = 0;
+    /**
+     * slide to index
+     * @param {number} index
+     */
+    this.slideTo = function(index) {
+      if(index < 0) {
+        window.scrollTo(0, 0);
+      } else if(this.limitIndex < index) {
+        window.scrollTo(0, this.offsetList[this.limitIndex]);
+      } else {
+        window.scrollTo(0, this.offsetList[index]);
+      }
+    };
+    /**
+     * slide to prev
+     */
+    this.prev = function() {
+      this.currentIndex--;
+      if(this.currentIndex < 0) {
+        this.currentIndex = 0;
+      }
+      this.slideTo(this.currentIndex);
+    };
+    /**
+     * slide to prev
+     */
+    this.next = function() {
+      this.currentIndex++;
+      if(this.limitIndex < this.currentIndex) {
+        this.currentIndex = this.limitIndex;
+      }
+      this.slideTo(this.currentIndex);
+    };
+  }
 
-	(function() {
-		//if here is not gist@github
-		if(win.location.hostname != 'gist.github.com') {
-			return;
-		}
+  (function() {
+    //if here is not gist@github
+    if(win.location.hostname != 'gist.github.com') {
+      return;
+    }
 
-		var start = Date.now();
-		console.log("[gistslide]start:" + (Date.now() - start));
+    //add .gs-theme to body
+    addClass(document.body, "gs-theme");
 
-		//add .gs-theme to body
-		addClass(document.body, "gs-theme");
+    //add .gs-slide to article.markdown-body
+    addClass(qs(".markdown-body"), "gs-slide");
 
-		//add .gs-slide to article.markdown-body
-		addClass(qs(".markdown-body"), "gs-slide");
+    //remove classes
+    removeClass(qs(".gs-slide"), "markdown-body js-file js-task-list-container is-task-list-enabled");
 
-		//remove classes
-		removeClass(qs(".gs-slide"), "markdown-body js-file js-task-list-container is-task-list-enabled");
+    //move article.markdown-body to body
+    moveNode(qs(".gs-slide"), document.body);
 
-		//move article.markdown-body to body
-		moveNode(qs(".gs-slide"), document.body);
+    //hide elements
+    addClass(byId("wrapper"), "is-hidden");
+    addClass(byId("ajax-error-message"), "is-hidden");
+    addClass(qs(".js-task-list-field"), "is-hidden");
+    addClass(qs("footer"), "is-hidden");
 
-		//hide elements
-		addClass(byId("wrapper"), "is-hidden");
-		addClass(byId("ajax-error-message"), "is-hidden");
-		addClass(qs(".js-task-list-field"), "is-hidden");
-		addClass(qs("footer"), "is-hidden");
+    //get headers under .gs-slide
+    var slideParent = qs(".gs-slide");
+    var header1 = slice.call(byTag('h1', slideParent));
+    var header2 = slice.call(byTag('h2', slideParent));
+    var headers = header1.concat(header2);
+    //filter element which has secret class
+    headers = filter.call(headers, function(header) {
+      return !header.classList.contains("secret");
+    });
 
-		//get headers under .gs-slide
-		var slideParent = qs(".gs-slide");
-		var header1 = slice.call(byTag('h1', slideParent));
-		var header2 = slice.call(byTag('h2', slideParent));
-		var headers = header1.concat(header2);
-		//filter element which has secret class
-		headers = filter.call(headers, function(header) {
-			return !header.classList.contains("secret");
-		});
+    //wrap headers with section.gs-slide-content
+    forEach.call(headers, function(header) {
+      //wrap header with section.gs-slide-content
+      wrapNode(header, createNode("section", {
+        class: "gs-slide-content"
+      }));
+    });
 
-		//wrap headers with section.gs-slide-content
-		forEach.call(headers, function(header) {
-			//wrap header with section.gs-slide-content
-			wrapNode(header, createNode("section", {
-				class: "gs-slide-content"
-			}));
-		});
+    //move following elements to section.gs-slide-content
+    var slides = byClass("gs-slide-content", qs(".gs-slide"));
+    forEach.call(slides, function(slide) {
+      var moveNodeList = [];
+      var element = getSibling(slide);
+      while(element && !element.classList.contains("gs-slide-content")) {
+        moveNodeList.push(element);
+        element = getSibling(element);
+      }
+      forEach.call(moveNodeList, function(node) {
+        moveNode(node, slide);
+      });
+    });
 
-		//move following elements to section.gs-slide-content
-		var slides = byClass("gs-slide-content", qs(".gs-slide"));
-		forEach.call(slides, function(slide) {
-			var moveNodeList = [];
-			var element = getSibling(slide);
-			while(element && !element.classList.contains("gs-slide-content")) {
-				moveNodeList.push(element);
-				element = getSibling(element);
-			}
-			forEach.call(moveNodeList, function(node) {
-				moveNode(node, slide);
-			});
-		});
+    //get hash string
+    var cssFile;
+    var hashValue = win.location.hash.replace('#', '');
+    if(hashValue !== "") {
+      cssFile = hashValue;
+    } else if(win.gistSlideTheme !== undefined) {
+      cssFile = win.gistSlideTheme;
+    }
+    if(!cssFile) {
+      cssFile = "default";
+    }
 
-		//get hash string
-		var cssFile;
-		var hashValue = win.location.hash.replace('#', '');
-		if(hashValue !== "") {
-			cssFile = hashValue;
-		} else if(win.gistSlideTheme !== undefined) {
-			cssFile = win.gistSlideTheme;
-		}
-		if(!cssFile) {
-			cssFile = "default";
-		}
+    //create link node
+    var linkNode = createNode('link', {
+      rel: 'stylesheet',
+      type: 'text/css',
+      href: 'https://gistslide.herokuapp.com/src/css/themes/' + cssFile + '.css'
+    });
 
-		//create link node
-		var linkNode = createNode('link', {
-			rel: 'stylesheet',
-			type: 'text/css',
-			href: 'https://gistslide.herokuapp.com/src/css/themes/' + cssFile + '.css'
-		});
+    linkNode.addEventListener("load", function() {
+      //wait for reflow completion
+      window.setTimeout(function() {
+        //after repaint
+        var container = new SlideContainer(byClass("gs-slide-content", qs(".gs-slide")));
 
-		linkNode.addEventListener("load", function() {
-			//wait for reflow completion
-			window.setTimeout(function() {
-				//after repaint
-				var container = new SlideContainer(byClass("gs-slide-content", qs(".gs-slide")));
+        //listen keydown event
+        doc.addEventListener('keydown', function(e) {
+          var keyCode = e.keyCode;
+          if(keyCode === KEYCODE_ENTER) {
+            if(e.shiftKey) {
+              container.prev();
+            } else {
+              container.next();
+            }
+          } else if(keyCode == KEYCODE_LEFT) {
+            container.prev();
+          } else if(keyCode == KEYCODE_RIGHT) {
+            container.next();
+          }
+        });
+      }, 1000);
+    });
 
-				//listen keydown event
-				doc.addEventListener('keydown', function(e) {
-					var keyCode = e.keyCode;
-					if(keyCode === KEYCODE_ENTER) {
-						if(e.shiftKey) {
-							container.prev();
-						} else {
-							container.next();
-						}
-					} else if(keyCode == KEYCODE_LEFT) {
-						container.prev();
-					} else if(keyCode == KEYCODE_RIGHT) {
-						container.next();
-					}
-				});
-			}, 1000);
-		});
-
-		//insert nodes into head tail
-		qs('head').appendChild(linkNode);
-		console.log("[gistslide]end:" + (Date.now() - start));
-	})();
+    //insert nodes into head tail
+    qs('head').appendChild(linkNode);
+  })();
 })();
